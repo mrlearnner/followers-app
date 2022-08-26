32f5c28d578e49f0cd9b55b3f21c2b147e76647a
Learn Ner <mrlearnner@gmail.com>
2022-08-26 16:21:47 -0400

    change remote distination

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/deploy-demo'));
+app.use(express.static(__dirname + '/dist/followers-app'));
 
 // Link index.html of build folder with router.
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/deploy-demo/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/followers-app/index.html'));
 });
 
 
